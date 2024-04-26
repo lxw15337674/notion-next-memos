8ad6400bf69278646fdb22a35576241fe86e0049
Bhwa233 <404174262@qq.com>
2024-04-26 17:57:14 +0800

    Refactor Editor component to use ahooks and add submit functionality in index.tsx

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -8,41 +8,46 @@ import TagSuggestions from './TagSuggestions';
 import Box from '@mui/joy/Box';
 import { Button } from '../ui/button';
 import useTagStore from '@/store/tag';
+import { useRequest } from 'ahooks';
+import { Loader2 } from 'lucide-react';
 
 const Editor = () => {
   const { insertMemo } = useMemoStore();
   const { fetchTags } = useTagStore();
   const editorRef = useRef<HTMLTextAreaElement | null>(null);
+  const editor = editorRef.current;
   const insertText = (text: string, offset = 0) => {
-    const current = editorRef?.current?.children?.[0] as HTMLTextAreaElement;
-    if (current) {
-      if (current === null) return;
-      const value = current.value;
-      const start = current.selectionStart;
-      const end = current.selectionEnd;
+    if (editor) {
+      const value = editor.value;
+      const start = editor.selectionStart;
+      const end = editor.selectionEnd;
       const newValue = value.slice(0, start) + `${text} ` + value.slice(end);
-      current.value = newValue;
+      editor.value = newValue;
       setTimeout(() => {
-        current.selectionStart = start + text.length + offset;
-        current.selectionEnd = start + text.length + offset;
-        current.focus();
+        editor.selectionStart = start + text.length + offset;
+        editor.selectionEnd = start + text.length + offset;
+        editor.focus();
         // 触发事件
         const event = new Event('input');
-        current.dispatchEvent(event);
+        editor.dispatchEvent(event);
       }, 50);
     }
   }
+  const { run:createRecord,loading} = useRequest(createPageInDatabase,{
+    manual:true,
+    onSuccess:(data)=>{
+      insertMemo(data, 0);
+      fetchTags();
+      editor!.value = '';
+    }
+  })
   const onSave = async () => {
-    const editor = editorRef?.current?.children?.[0] as HTMLTextAreaElement;
     if (!editor) {
       return
     }
     const content = editor.value ?? '';
     if (content.trim().length === 0) return;
-    const data = await createPageInDatabase(content);
-    insertMemo(data, 0);
-    fetchTags();
-    editor.value = '';
+     await createRecord(content);
   };
   return (
     <div className="relative">
@@ -64,13 +69,16 @@ const Editor = () => {
               <Icon.Hash size={16} />
             </Button>
             <Button
+              disabled={loading}
               variant="outline"
               size="icon"
               type='submit'
               onClick={onSave}
               className="ml-auto w-16 h-8"
             >
-              <Icon.Send size={20} className="" />
+              {
+                loading ? <Loader2 size={20} className="animate-spin" /> : <Icon.Send size={20}  />
+              }
             </Button>
           </Box>
         }
