c6637f74679855324c7f3acffac88309fb08b3e5
Bhwa233 <404174262@qq.com>
2024-05-23 14:02:28 +0800

    refactor: Move NotionAPI instantiation to service action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,10 @@ import MobileHeader from '@/components/MobileHeader';
 import LeftSide from '@/components/LeftSide';
 
 export default async function Home() {
-  // 这个地方不知道为什么，放到service action里面会build报错，所以只能放到这里
+  // 这个地方不知道为什么，放到service action里面会build报错：
   // ./node_modules/.pnpm/p-queue@7.4.1/node_modules/p-queue/dist/index.js + 4 modules
   // Cannot get final name for export 'EventEmitter' of./ node_modules /.pnpm / eventemitter3@5.0.1 / node_modules / eventemitter3 / index.mjs
+  // 所以只能放到这里
   const api = new NotionAPI({
     activeUser: process.env.NOTION_ACTIVE_USER,
     authToken: process.env.NOTION_TOKEN_V2,
