01e4978a9163e20a29f096f9d4fb0eecc9c03150
Bhwa233 <404174262@qq.com>
2024-05-24 18:06:27 +0800

    wip:react-mentions

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,120 +1,113 @@
 'use client';
-import React, { useRef, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+import { MentionsInput, Mention } from 'react-mentions';
 import { Textarea } from '@mui/joy';
 import Icon from '../Icon';
-import TagSuggestions from './TagSuggestions';
-import Box from '@mui/joy/Box';
-import { Button } from '../ui/button';
 import useTagStore from '@/store/tag';
 import { Loader2 } from 'lucide-react';
-import { useDebounceFn, useKeyPress } from 'ahooks';
+import { Button } from '../ui/button';
+import Box from '@mui/joy/Box';
+import { useDebounceFn } from 'ahooks';
+import defaultStyle from './defaultStyle'
 
 interface Props {
   onSubmit: (text: string) => Promise<any>;
   onCancel?: () => void;
   defaultValue?: string;
 }
-export interface ReplaceTextFunction {
-  (text: string, start: number, end: number, cursorOffset?: number): void
-}
 
 const Editor = ({ onSubmit, defaultValue, onCancel }: Props) => {
-  const { fetchTags } = useTagStore();
+  const { tags, fetchTags } = useTagStore();
   const [loading, setLoading] = React.useState(false);
-  const [editorRef, setEditorRef] = useState<HTMLTextAreaElement | null>(null);
-  const { run: replaceText } = useDebounceFn<ReplaceTextFunction>((text, start, end, offset = 0) => {
-    const editor = editorRef;
-    if (editor) {
-      const value = editor.value;
-      const newValue = value.slice(0, start) + `${text} ` + value.slice(end);
-      editor.value = newValue;
-      setTimeout(() => {
-        editor.selectionStart = start + text.length + offset;
-        editor.selectionEnd = start + text.length + offset;
-        editor.focus();
-        // 触发事件
-        const event = new Event('input');
-        editor.dispatchEvent(event);
-      }, 100);
+  const [mentionValue, setMentionValue] = useState('');
+  const editorRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const onSave = useCallback(async () => {
+    if (!editorRef.current) {
+      return;
     }
-  }, { wait: 200 });
-  const onSave = async () => {
-    const editor = editorRef;
-    if (!editor) {
-      return
-    }
-    const content = editor.value ?? '';
+    const content = editorRef.current.value ?? '';
     if (content.trim().length === 0) return;
     setLoading(true);
     await onSubmit?.(content).finally(() => {
       setLoading(false);
-    })
-    fetchTags()
-    editor!.value = '';
-  };
-  useKeyPress('ctrl.enter', (e) => {
-    // 判断是否focus
-    if (editorRef === document.activeElement) {
+    });
+    fetchTags();
+    editorRef.current!.value = '';
+  }, [fetchTags, onSubmit]);
+
+  const handleKeyDown = useCallback((e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && (e.ctrlKey || e.metaKey)) {
       e.preventDefault();
       e.stopPropagation();
       onSave();
     }
-  });
+  }, [onSave]);
+
+
   return (
     <div className="relative">
-      <Textarea
-        className="w-full h-full bg-card text-card-foreground"
+      <MentionsInput
+        value={mentionValue}
+        onChange={(event) => setMentionValue(event.target.value)}
+        className="w-full h-full bg-card text-card-foreground min-h-[63px] p-2"
         placeholder="此刻的想法..."
-        minRows={3}
-        autoFocus
+        // Remove the inputComponent prop
+        // inputComponent={Textarea}
+        // style={defaultStyle}
+        autoFocus={true}
         defaultValue={defaultValue}
-        slotProps={{ textarea: { ref: setEditorRef } }}
-        endDecorator={
-          <Box
-            className="py-1 border-t  flex items-center flex-auto "
+        onKeyDown={handleKeyDown}
+        inputRef={editorRef}
+        allowSuggestionsAboveCursor
+        allowSpaceInQuery
+      >
+        <Mention
+          trigger="#"
+          data={tags.map((tag) => ({ id: tag.id, display: tag.name }))}
+          appendSpaceOnAdd
+        />
+      </MentionsInput>
+      <Box
+        className="py-1 border-t  flex items-center flex-auto "
+      >
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => {
+            if (!editorRef.current) {
+              return;
+            }
+          }
+          }
+        >
+          <Icon.Hash size={16} />
+        </Button>
+        <div className="flex items-center ml-auto">
+          {onCancel && <Button
+            disabled={loading}
+            variant="ghost"
+            size='icon'
+            onClick={onCancel}
+            className="w-16 h-8"
           >
-            <Button
-              variant="ghost"
-              size="icon"
-              onClick={() => {
-                if (!editorRef) {
-                  return
-                }
-                replaceText('#', editorRef?.selectionStart, editorRef?.selectionStart, 0)
-              }
-              }
-            >
-              <Icon.Hash size={16} />
-            </Button>
-            <div className="flex items-center ml-auto">
-              {onCancel && <Button
-                disabled={loading}
-                variant="ghost"
-                size='icon'
-                onClick={onCancel}
-                className="w-16 h-8"
-              >
-                取消
-              </Button>
-              }
-              <Button
-                disabled={loading}
-                variant="outline"
-                size="icon"
-                type='submit'
-                onClick={onSave}
-                className="ml-4 w-16 h-8"
-              >
-                {
-                  loading ? <Loader2 size={20} className="animate-spin" /> : <Icon.Send size={20} />
-                }
-              </Button>
-            </div>
-
-          </Box>
-        }
-      />
-      <TagSuggestions editorRef={editorRef} replaceText={replaceText} />
+            取消
+          </Button>
+          }
+          <Button
+            disabled={loading}
+            variant="outline"
+            size="icon"
+            type='submit'
+            onClick={onSave}
+            className="ml-4 w-16 h-8"
+          >
+            {
+              loading ? <Loader2 size={20} className="animate-spin" /> : <Icon.Send size={20} />
+            }
+          </Button>
+        </div>
+      </Box>
     </div>
   );
 };
